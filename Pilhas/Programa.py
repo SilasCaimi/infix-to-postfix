7ef9e3764c79850276376a2f19572d3f8ec8b228
SilasCaimi <silascaimi@live.com>
2018-08-12 19:18:14 -0300

    Atualiza Programa.py
    Remove linhas de comentários para execução do programa

--- Pilhas/Programa.py
+++ Pilhas/Programa.py
@@ -2,10 +2,6 @@ from Pilha import *
 from Postfix import *
 from Avaliar import *
 
-#infixa = "3+4*(2$2$3+2)"
-
-#infixa = "((A+B)*C-(D-E))$(F+G)"
-
 infixa = input("Digite uma expressao infixa: ")
 
 posfixa = postfix(infixa)
